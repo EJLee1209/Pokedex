22817cbfdc65f04454c399f109c7c9e2c003861f
EJLee1209 <dldmswo1209@gmail.com>
2023-08-31 12:57:51 +0900

    Feat: 프로젝트 셋업, 테마 설정

---
+++ Pokedex/Theme/ThemeColor.swift
@@ -0,0 +1,30 @@
+//
+//  ThemeColor.swift
+//  Pokedex
+//
+//  Created by 이은재 on 2023/08/31.
+//
+
+import UIKit
+
+struct ThemeColor {
+    static let primary = UIColor(hexString: "D21312")
+    static let normal = UIColor(hexString: "D8D9DA")
+    static let fire = UIColor(hexString: "FF612B")
+    static let water = UIColor(hexString: "2892FF")
+    static let gross = UIColor(hexString: "47BF26")
+    static let thunder = UIColor(hexString: "F2CB05")
+    static let ice = UIColor(hexString: "62DFFF")
+    static let fight = UIColor(hexString: "F28705")
+    static let poison = UIColor(hexString: "9652D9")
+    static let ground = UIColor(hexString: "AA7939")
+    static let fly = UIColor(hexString: "95C8FF")
+    static let esper = UIColor(hexString: "FF637F")
+    static let bug = UIColor(hexString: "9FA423")
+    static let stone = UIColor(hexString: "BCB889")
+    static let ghost = UIColor(hexString: "6E4570")
+    static let dragon = UIColor(hexString: "5462D6")
+    static let devil = UIColor(hexString: "4F4747")
+    static let steel = UIColor(hexString: "6AAED3")
+    static let fairy = UIColor(hexString: "FFB1FF")
+}
